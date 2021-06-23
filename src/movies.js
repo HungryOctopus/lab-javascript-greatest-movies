45400c66c998ba8b280196da5e263d4b3e059b12
Amélie Monnoyer <amelie.monnoyer@gmail.com>
2021-06-23 22:23:44 +0200

    Done until iteration 4

--- src/movies.js
+++ src/movies.js
@@ -12,24 +12,31 @@ function getAllDirectors(movies) {
 
 // Bonus: no duplicates: with filter method
 // if this item is alredy there in the array, we won't include it
-const filteredDirectors = allMovieDirectors.filter(
-  (value, index, originalArray) => {
-    return originalArray.indexOf(value) === index;
-    // with indexOf (check if the value is the same as the index)
+
+function getAllUniqueDirectors(arr) {
+  let directors = arr.map((elem) => elem.director);
+  let uniques = [];
+  for (let i = 0; i < directors.length; i++) {
+    if (!uniques.includes(directors[i])) {
+      uniques.push(directors[i]);
+    }
   }
-);
-return filteredDirectors;
+  return arr;
+}
+// other possibility: with indexOf (check if the value is the same as the index)
 //you could chained it to the function getAllDirectors
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
 function howManyMovies(movies) {
-  const dramaMoviesDirectedBySpielberg = movies.filter((movie) => {
+  return movies.filter((movie) => {
     // return movie.director === 'Steven Spielberg' && movie.genre.includes 'Drama'
-    const spielbergMovies = movie.director === 'Steven Spielberg';
-    // const isMovieDrama
-  });
+    return (
+      movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+    );
+  }).length;
 }
+
 //to round to 2 decimals after the comma
 const round = (value, digits) =>
   Math.round(value * 10 ** digits) / 10 ** digits;
@@ -45,13 +52,21 @@ function scoresAverage(movies) {
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  const dramaMovies = movies.filter((movie) => {
+    return movie.genre.includes('Drama') && movie.score !== undefined;
+  });
+  const dramaMoviesScores =
+    dramaMovies.reduce((accumulator, movie) => {
+      accumulator + movie.score;
+    }, 0) / dramaMovies.length;
+  return round(dramaMoviesScores, 2);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
 // use .sort but clone the array before so the array won't be mutated
 //for alphabetical order: use localeCompare
-
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
@@ -61,12 +76,8 @@ function orderAlphabetically() {}
 
 // sort the array alphabetically & use localeCompare
 
-
-
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
-
-
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
